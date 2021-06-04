bf96dbcb33e05bd6fc3e84dc54f6c205072ec19f
Robert <pryadchenkor@gmail.com>
2021-06-04 14:50:36 +0300

    desktop version

---
+++ app/js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+  });
+  $(".rightside-menu-close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+
+  $(".top__slider").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+  });
+
+  $(".contact-slider").slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    dots: true,
+    autoplay: true,
+    arrows: false,
+  });
+
+  $(".article-slider-box").slick({
+    autoplay: true,
+    prevArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowleft "> <img src="images/slider-arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowright"> <img src="images/slider-arrow-right.svg" alt="arrow-right"></button>',
+  });
+
+  var mixer = mixitup(".gallery__inner", {
+    load: {
+      filter: ".living",
+    },
+  });
+});
